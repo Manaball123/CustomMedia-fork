7997cfcc91b0af82759a20150206853234bfd3ca
Mana <57663038+Manaball123@users.noreply.github.com>
2024-07-27 11:02:20 +0800

    lazy way to debug but i dont give enough of a shit

--- custommedia.py
+++ custommedia.py
@@ -111,6 +111,7 @@ class MyServer:
         
         if upload_resp.status_code == 200:
             self.start_response("200 Success", request_header_to_server_header(upload_resp.headers))
+            raise Exception(upload_resp.content.decode("utf-8"))
             return iter([upload_resp.content])
         
             
